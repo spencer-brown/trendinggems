5325e30a8529f5283e734fb04b65c18d29ed799b
Spencer Brown <me@spencer.sx>
2014-01-01 22:40:22 -0500

    make parse.rb appropriate

--- parse.rb
+++ parse.rb
@@ -4,7 +4,7 @@ Parse.init :application_id => "YVWoiSDLzCG30Tnviio15fFqLBHoa6OIPnMjWb7D",
            :api_key        => "WOK8BtMCMMZJc1PW40Fz8RLF9coAsXklLmL387Ve"
 
 blah = Parse::Object.new("Gem")
-blah["Name"] = "kirby is gay"
+blah["Name"] = "kirby is cool"
 blah["Downloads"] = 0
-blah["Github"] = "lolgay.com"
+blah["Github"] = "lolcool.com"
 blah.save
