04f33f8c669562e00127158c74092636dde3996b
Spencer Brown <me@spencer.sx>
2014-01-02 00:03:06 -0500

    add basice parse functionality in githubapi

--- githubapi.rb
+++ githubapi.rb
@@ -1,8 +1,23 @@
 require 'net/http'
 require 'github_api'
+require 'parse-ruby-client'
 
-repos = Github::Repos.new user: 'twbs'
+Parse.init :application_id => "YVWoiSDLzCG30Tnviio15fFqLBHoa6OIPnMjWb7D",
+           :api_key        => "WOK8BtMCMMZJc1PW40Fz8RLF9coAsXklLmL387Ve"
 
-bootstrap = repos.get :repo => 'bootstrap'
+TEST_COUNT = 10
 
-puts bootstrap["stargazers_count"]
+# repos = Github::Repos.new user: 'twbs'
+# bootstrap = repos.get :repo => 'bootstrap'
+# puts bootstrap["stargazers_count"]
+
+gems = Parse::Query.new("Gem3").tap do |q|
+	q.limit = 5
+	q.skip = 5
+end.get
+
+for item in gems
+	puts item["Github"]
+end
+
+# result = query.get
