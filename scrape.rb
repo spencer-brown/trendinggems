bef703d78229aee15c75c158d35f0cbe794c7d61
Spencer Brown <me@spencer.sx>
2014-01-01 22:41:20 -0500

    remove debug puts from scrape.rb again

--- scrape.rb
+++ scrape.rb
@@ -19,12 +19,10 @@ LETTERS_IN_ALPHA = 26
 
 LETTERS_IN_ALPHA.times do |i|
 	letter = (START_LETTER + i).chr
-	# puts "starting letter"
 
 	first_page = Nokogiri::HTML(open("http://rubygems.org/gems?letter=A&page=1"))
 	total_number_gems = first_page.xpath("/html/body/div/div[3]/div/p/b[2]").text.to_f
 	number_of_pages = (total_number_gems/GEMS_PER_PAGE).ceil
-	# puts "Hello World"
 	for page in 1..number_of_pages
 		STDERR.write "Scraping page #{page} for letter #{letter}\n"
 		doc = Nokogiri::HTML(open("http://rubygems.org/gems?letter=#{letter}&page=#{page}"))
